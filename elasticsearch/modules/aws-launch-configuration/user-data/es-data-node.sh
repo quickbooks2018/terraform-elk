c3514a511ba3d215059bd74042c3096c8fdc34a9
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-11-10 06:21:24 +0500

    Update es-data-node.sh

--- elasticsearch/modules/aws-launch-configuration/user-data/es-data-node.sh
+++ elasticsearch/modules/aws-launch-configuration/user-data/es-data-node.sh
@@ -12,7 +12,7 @@
 # Elastic DATA Nodes
 ####################
 ELASTIC_IMAGE='docker.elastic.co/elasticsearch/elasticsearch'
-ELASTIC_VERSION='7.17.7'
+ELASTIC_VERSION='7.17.6'
 HOST1='elasticsearch-node1.cloudgeeks.tk'
 HOST2='elasticsearch-node2.cloudgeeks.tk'
 HOST3='elasticsearch-node3.cloudgeeks.tk'
