e2155e5e08eee4fee7a11ad23501db36f8ec2591
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-11-10 13:06:09 +0500

    Update es-monitoring.sh

--- elasticsearch/modules/aws-launch-configuration/user-data/es-monitoring.sh
+++ elasticsearch/modules/aws-launch-configuration/user-data/es-monitoring.sh
@@ -32,7 +32,7 @@ export KIBANA
 cat <<EOF > $PWD/kibana.yml
 elasticsearch.username: "elastic"
 elasticsearch.password: "cloudgeeks"
-elasticsearch.hosts: [ "https://elasticsearch-cluster.cloudgeeks.tk" ]
+elasticsearch.hosts: "https://elasticsearch-cluster.cloudgeeks.tk"
 EOF
 
 cat <<EOF > Dockerfile
