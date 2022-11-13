04eb8703e6f427176c8fd5159cc75dce2319f9a9
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-11-13 18:20:15 +0500

    Update es-master-2.sh

--- elasticsearch/modules/aws-launch-configuration/user-data/es-master-2.sh
+++ elasticsearch/modules/aws-launch-configuration/user-data/es-master-2.sh
@@ -63,6 +63,7 @@ EOF
 ############
 # APM Server
 ############
+KIBANA_URL='kiabana.cloudgeeks.tk'
 # https://raw.githubusercontent.com/elastic/apm-server/master/apm-server.docker.yml
 cat << EOF > apm-server.yml
 ---
@@ -73,6 +74,9 @@ apm-server:
 output.elasticsearch:
   hosts: ["http://${localip}:9200"]
 
+kibana:
+  enabled: true
+  host: ["http://${KIBANA_URL}:5601"]
 
 monitoring:
   enabled: true
