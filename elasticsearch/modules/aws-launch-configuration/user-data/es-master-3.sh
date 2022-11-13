beb30192848fab878f7cb23b7ca951a39a13043a
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-11-13 17:17:35 +0500

    Update es-master-3.sh

--- elasticsearch/modules/aws-launch-configuration/user-data/es-master-3.sh
+++ elasticsearch/modules/aws-launch-configuration/user-data/es-master-3.sh
@@ -53,29 +53,30 @@ metricbeat.modules:
   enabled: true
 
 processors:
-  - add_cloud_metadata: ~
-
+  - add_cloud_metadata: ~ ' > $PWD/metricbeat.yml
+cat << EOF >> metricbeat.yml
 output.elasticsearch:
-  hosts: 'http://:elasticsearch:9200' ' > $PWD/metricbeat.yml
-
+  hosts: 'http://${localip}:9200'
+EOF
 
 
 ############
 # APM Server
 ############
 # https://raw.githubusercontent.com/elastic/apm-server/master/apm-server.docker.yml
-echo '
+cat << EOF > apm-server.yml
 ---
 apm-server:
   host: 0.0.0.0:8200
   ssl.enabled: false
 
 output.elasticsearch:
-  hosts: ["http://elasticsearch:9200"]
+  hosts: ["http://${localip}:9200"]
 
 
 monitoring:
-  enabled: true '  > apm-server.yml
+  enabled: true
+EOF
 
 
 #################
@@ -184,6 +185,7 @@ services:
         context: .
         dockerfile: MetricBeatDockerfile
       image: metricbeat:metricbeat
+      network_mode: host
       container_name: metricbeat
       restart: unless-stopped
       depends_on: ['elasticsearch']
@@ -201,6 +203,7 @@ services:
      context: .
      dockerfile: APMServerDockerfile
     image: apm:apm
+    network_mode: host
     depends_on: ['elasticsearch']
     container_name: apm
     command: -e --strict.perms=false
